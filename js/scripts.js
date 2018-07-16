eafbb4e4fcbb195ffdff54e3e852994d61889f01
cynthiaokello <cynthiaokello050@gmail.com>
2018-07-16 09:51:39 +0300

    made changes to the content

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,37 @@
+
+//Business Interface
+var cynthia=[];
+function user(result) {
+  for(index=1;index<=result;index++){
+
+
+    if (index % 15 === 0){
+      cynthia.push("pingpong");
+    }
+    else if (index % 3 === 0) {
+      cynthia.push("ping");
+    }
+    else if (index % 5 === 0) {
+      cynthia.push("pong");
+    }
+    else {
+      my.push(index);
+    }
+  }
+    }
+
+
+
+
 //User Interface
 $(document).ready(function() {
-    $("form").click(function(event) {
+    $("form").submit(function(event) {
       event.preventDefault();
       var number = $("#number").val();
       var result =parseInt(number);
       user(result);
       cynthia.forEach(function(result) {
-        $("#outcome").append("<li>" + result + "</li>");
+        $("#result").append("<li>" + result + "</li>");
       });
     });
    });
-
-
-//Business Interface
-   var cynthia=[];
-    function user(result) {
-      for(index=1;index<=result;index++){
-
-
-        if (index % 15 === 0){
-          cynthia.push("pingpong");
-        }
-        else if (index % 3 === 0) {
-          cynthia.push("ping");
-        }
-        else if (index % 5 === 0) {
-          cynthia.push("pong");
-        }
-        else {
-          cynthia.push(index);
-        }
-      }
-      }
